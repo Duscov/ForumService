a523244f427b2d9103a7cacfb22d933a1d136781
dimi <dmduscov@gmail.com>
2025-10-06 11:33:16 +0200

    security init

---
+++ src/main/java/cohort_65/java/forumservice/security/SequrityConfiguration.java
@@ -0,0 +1,23 @@
+package cohort_65.java.forumservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SequrityConfiguration {
+
+    @Bean
+    SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/account/register").permitAll()
+                .anyRequest().authenticated());
+        return http.build();
+    }
+}
